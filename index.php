98172052b916eb3a63a6b4f0acca1054317e9b7e
vlad-krechun <creciunvladislav@gmail.com>
2020-12-08 21:09:48 +0200

    add img  temporary  links  for project

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		<div class="phone__wrapper">
 			<div class="phone">
 				<a href="tel:+37367111905" class="phone__link">
-					<img src="img/phone.png" alt="" class="phone__img">
+					<img src="<?php echo bloginfo("template_url");?>/assets/img/phone.png" alt="" class="phone__img">
 				</a></div>
 		</div>
 
@@ -28,7 +28,7 @@
 				<div class="header__body">
 
 					<!-- <a href="#" class="header__logo">
-							<img src="img/logo.jpg" alt="">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/logo.jpg" alt="">
 						</a> -->
 					<div class="header__burger">
 
@@ -55,7 +55,7 @@
 							<li>
 								<a href="" class="header__link">
 									<span class="lang">
-										<img src="img/lang.png" alt="" class="lang__logo">
+										<img src="<?php echo bloginfo("template_url");?>/assets/img/lang.png" alt="" class="lang__logo">
 										Ru
 									</span>
 								</a>
@@ -66,7 +66,7 @@
 						<input type="search" id="site-search" name="q" aria-label="Search through site content"
 							class="search__input">
 						<button class="search__button">
-							<img src="img/search.png" alt="Caută" class="search__img">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/search.png" alt="Caută" class="search__img">
 						</button>
 
 					</div>
@@ -74,7 +74,7 @@
 			</div>
 		</header>
 
-		<div class="fullscreen fullscreen_one" style="background-image:url('img/sunflower.jpg')">
+		<div class="fullscreen fullscreen_one" style="background-image:url('<?php echo bloginfo("template_url");?>/assets/img/sunflower.jpg')">
 			<div class="fullscreen__body">
 				<span class="space">
 					<a href="#about__oil" class="fullscreen__link">
@@ -101,7 +101,7 @@
 			<div class="seed">
 				<a href="#" class="seed__link">
 					<div class="seed__one">
-						<div class="wrapper__seed__img"> <img src="img/dovleac.jpg" alt="De dovleac" class="seed__img">
+						<div class="wrapper__seed__img"> <img src="<?php echo bloginfo("template_url");?>/assets/img/dovleac.jpg" alt="De dovleac" class="seed__img">
 						</div>
 						<span class="seed__text">De dovleac</span>
 					</div>
@@ -109,7 +109,7 @@
 				<a href="#" class="seed__link">
 					<div class="seed__one">
 						<div class="wrapper__seed__img">
-							<img src="img/in.jpg" alt="De In" class="seed__img">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/in.jpg" alt="De In" class="seed__img">
 						</div>
 
 						<span class="seed__text">De In</span>
@@ -118,7 +118,7 @@
 				<a href="#" class="seed__link">
 					<div class="seed__one">
 						<div class="wrapper__seed__img">
-							<img src="img/susan.jpg" alt="De Susan" class="seed__img">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/susan.jpg" alt="De Susan" class="seed__img">
 						</div>
 
 						<span class="seed__text">De Susan</span>
@@ -127,7 +127,7 @@
 				<a href="#" class="seed__link">
 					<div class="seed__one">
 						<div class="wrapper__seed__img">
-							<img src="img/chimen_negru.jpg" alt="De chimen Negru" class="seed__img">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/chimen_negru.jpg" alt="De chimen Negru" class="seed__img">
 						</div>
 						<span class="seed__text">De chimen Negru</span>
 					</div>
@@ -135,7 +135,7 @@
 				<a href="#" class="seed__link">
 					<div class="seed__one">
 						<div class="wrapper__seed__img">
-							<img src="img/cinepa.jpg" alt="De cînepă" class="seed__img">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/cinepa.jpg" alt="De cînepă" class="seed__img">
 						</div>
 						<span class="seed__text">De cînepă</span>
 					</div>
@@ -143,7 +143,7 @@
 				<a href="#" class="seed__link">
 					<div class="seed__one">
 						<div class="wrapper__seed__img">
-							<img src="img/armurariu.jpg" alt="De armurariu" class="seed__img">
+							<img src="<?php echo bloginfo("template_url");?>/assets/img/armurariu.jpg" alt="De armurariu" class="seed__img">
 						</div>
 						<span class="seed__text">De armurariu</span>
 					</div>
@@ -158,7 +158,7 @@
 			</div>
 		</div>
 		<div class="articles">
-			<img src="img/articles_1.jpg" alt="" class="articles__img articles__element">
+			<img src="<?php echo bloginfo("template_url");?>/assets/img/articles_1.jpg" alt="" class="articles__img articles__element">
 			<div class="articles__small articles__element" id="about__oil">
 				<h2 class="articles__title">Beneficiile uleiului viu</h2>
 				<p class="articles__text">Uleiul vegetal, care a fost obținut prin metoda tradițională (presare manuală a
@@ -170,7 +170,7 @@
 					<span class="articles__buttons">Citește mai departe...</span>
 				</a>
 			</div>
-			<img src="img/articles_2.jpg" alt="" class="articles__img articles__element">
+			<img src="<?php echo bloginfo("template_url");?>/assets/img/articles_2.jpg" alt="" class="articles__img articles__element">
 			<div class="articles__small articles__element" id="storage__oil">
 				<h2 class="articles__title">Cum sa păstrezi uleiul?</h2>
 				<p class="articles__text">Pentru a păstra uleiul viu este nevoie de respectat cîteva reguli: Feriți de
@@ -193,12 +193,12 @@
 						<h3 class="social__title">Sau scrieți-ne pe rețele de socializare:</h3>
 						<span class="facebook">
 							<a href="#" class="social__link">
-								<img src="img/facebook  1.jpg" alt="facebook" class="social__img">
+								<img src="<?php echo bloginfo("template_url");?>/assets/img/facebook  1.jpg" alt="facebook" class="social__img">
 							</a>
 						</span>
 						<span class="telegram">
 							<a href="#" class="social__link">
-								<img src="img/telegram .png" alt="telegram" class="social__img">
+								<img src="<?php echo bloginfo("template_url");?>/assets/img/telegram .png" alt="telegram" class="social__img">
 							</a>
 						</span>
 
@@ -249,7 +249,7 @@
 			</div>
 			<div class="footer__about">
 				<h3 class="footer__about__title">Despre noi:</h3>
-				<img src="img/about.jpg" alt="Despre noi">
+				<img src="<?php echo bloginfo("template_url");?>/assets/img/about.jpg" alt="Despre noi">
 			</div>
 		</div>
 
