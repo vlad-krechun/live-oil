6783ee31ccda7daa7f589aea9811f5c217c81d46
vlad-krechun <creciunvladislav@gmail.com>
2020-12-12 13:12:31 +0200

    mini pausein building blog

--- blog.php
+++ blog.php
@@ -26,6 +26,77 @@ get_header();
 		<div class="articles">
 			<div class="article">
 				<div class="img_prev">
+					<img src="<?php echo bloginfo("template_url"); ?>/assets/img/in.jpg" alt="">
+				</div>
+				<div class="article_text">
+					<h2 class="article_title">Lorem ipsum dolor sit amet consectetur</h2>
+					<div class="article_category">
+						<a href="#" class="category_link">Category</a>
+						<a href="#" class="category_link">Category</a>
+
+					</div>
+					<div class="article_label">
+						<a href="#" class="article__label-single">Label 1</a>
+						<a href="#" class="article__label-single">Label 2</a>
+						<a href="#" class="article__label-single">Label 3</a>
+						<a href="#" class="article__label-single">Label 4</a>
+
+					</div>
+					<p class="article_prev"> Tdipisicing elit. Distinctio, laborum consequatur velit assumenda iure
+						asperiores, in, facilis modi cumque eius architecto? Itaque pariatur sed ab omnis veniam. Obcaecati,adipisicing elit. Distinctio, laborum consequatur velit assumenda iure
+						asperiores, in, facilis modi cumque eius architecto? Itaque pariatur sed ab omnis veniam. Obcaecati,
+						omnis rerum.</p>
+					<a href="#" class="articles__link">
+						<span class="articles__buttons">Citește mai departe...</span>
+					</a>
+				</div>
+			</div>
+			<div class="article">
+				<div class="img_prev">
+					<img src="<?php echo bloginfo("template_url"); ?>/assets/img/image_1-blog.jpg" alt="">
+				</div>
+				<div class="article_text">
+					<h2 class="article_title">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="article_prev">, adipisicing elit. Distinctio, laborum consequatur velit assumenda iure
+						asperiores, in, facilis modi cumque eius architecto? Itaque pariatur sed ab omnis veniam. Obcaecati,
+						omnis rerum.</p>
+				</div>
+			</div>
+			<div class="article">
+				<div class="img_prev">
+					<img src="<?php echo bloginfo("template_url"); ?>/assets/img/image_1-blog.jpg" alt="">
+				</div>
+				<div class="article_text">
+					<h2 class="article_title">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="article_prev">, adipisicing elit. Distinctio, laborum consequatur velit assumenda iure
+						asperiores, in, facilis modi cumque eius architecto? Itaque pariatur sed ab omnis veniam. Obcaecati,
+						omnis rerum.</p>
+				</div>
+			</div>
+			<div class="article">
+				<div class="img_prev">
+					<img src="<?php echo bloginfo("template_url"); ?>/assets/img/image_1-blog.jpg" alt="">
+				</div>
+				<div class="article_text">
+					<h2 class="article_title">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="article_prev">, adipisicing elit. Distinctio, laborum consequatur velit assumenda iure
+						asperiores, in, facilis modi cumque eius architecto? Itaque pariatur sed ab omnis veniam. Obcaecati,
+						omnis rerum.</p>
+				</div>
+			</div>
+			<div class="article">
+				<div class="img_prev">
+					<img src="<?php echo bloginfo("template_url"); ?>/assets/img/image_1-blog.jpg" alt="">
+				</div>
+				<div class="article_text">
+					<h2 class="article_title">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="article_prev">, adipisicing elit. Distinctio, laborum consequatur velit assumenda iure
+						asperiores, in, facilis modi cumque eius architecto? Itaque pariatur sed ab omnis veniam. Obcaecati,
+						omnis rerum.</p>
+				</div>
+			</div>
+			<div class="article">
+				<div class="img_prev">
 					<img src="<?php echo bloginfo("template_url"); ?>/assets/img/image_1-blog.jpg" alt="">
 				</div>
 				<div class="article_text">
